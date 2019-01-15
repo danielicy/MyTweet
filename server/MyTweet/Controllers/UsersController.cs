930fa79838df1203520ff2d5596c315f77c5d0b0
Danieli <danielic@razorgrip.com>
2019-01-15 20:31:10 +0200

    added UsersController

---
+++ server/MyTweet/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MyTweet.Helpers;
+
+namespace MyTweet.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public UsersController(           
+            IMapper mapper,
+            IOptions<AppSettings> appSettings)
+        {          
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+        }
+    }
+}
