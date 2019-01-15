8382f32088c1627db2ad31166ee30ef72ced0727
Danieli <danielic@razorgrip.com>
2019-01-15 20:40:47 +0200

    data models

---
+++ server/DataModels/Models/UserManagment/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+public class Role
+{
+    [Column("role_id")]
+    public int RoleId { get; set; }
+
+    [Column("role_name")]
+    public string RoleName { get; set; }
+}
+}
