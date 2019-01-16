dfecc39b7d5e94063a045527dddebe3aa3345cac
danielic@razorgrip.com <c@e>
2019-01-16 10:10:33 +0200

    Tweet client service

---
+++ client/src/app/tweetmodule/_services/tweet.servicet.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Tweet } from '@/_models';
+
+@Injectable({ providedIn: 'root' })
+export class TweetsService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<Tweet[]>(`${config.apiUrl}/tweets`);
+    }
+
+    getById(id: number) {
+        return this.http.get(`${config.apiUrl}/tweets/${id}`);
+    }
+
+    tweet(tweet: Tweet) {
+        return this.http.post(`${config.apiUrl}/tweets/register`, tweet);
+    }
+
+    update(tweet: Tweet) {
+        return this.http.put(`${config.apiUrl}/tweets/${tweet.id}`, tweet);
+    }
+
+    
+}
