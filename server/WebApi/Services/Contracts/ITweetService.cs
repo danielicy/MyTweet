d3f68f6d66109e2e512429cb83b04bca1d506e2a
danielic@razorgrip.com <c@e>
2019-01-16 09:38:03 +0200

    Contracts

---
+++ server/WebApi/Services/Contracts/ITweetService.cs
@@ -0,0 +1,13 @@
+﻿using DataModels.Models.Tweets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTweetAPI.Services.Contracts
+{
+    public interface ITweetService : ICRUDBase<Tweet>
+    {
+        Tweet Tweet(string content);
+    }
+}
