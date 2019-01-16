74909c46ab5da8bdf4492bce16fa02eeebcc6563
danielic@razorgrip.com <c@e>
2019-01-16 10:11:38 +0200

    TweetsController ctor

--- server/WebApi/Controllers/TweetsController.cs
+++ server/WebApi/Controllers/TweetsController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MyTweetAPI.Services.Contracts;
+using WebApi.Helpers;
 
 namespace MyTweetAPI.Controllers
 {
@@ -11,5 +15,19 @@ namespace MyTweetAPI.Controllers
     [ApiController]
     public class TweetsController : ControllerBase
     {
+        private ITweetService _tweetsService;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public TweetsController(
+            ITweetService tweetsService,
+            IMapper mapper,
+            IOptions<AppSettings> appSettings)
+        {
+            _tweetsService = tweetsService;
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+        }
+
     }
 }
