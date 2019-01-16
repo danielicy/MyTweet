070c7fa26a176001d82a0d71390cf96e05b1d601
danielic@razorgrip.com <c@e>
2019-01-16 18:29:55 +0200

    class Follower

---
+++ server/DataModels/Models/UserManagment/Follower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataModels.Models.UserManagment
+{ 
+    [Table("followers")]
+    public class Follower
+    {
+        [Key, Column("id")]
+        public int Id { get; set; }
+
+        [Column("userid")]
+        public int UserId { get; set; }
+
+        [Column("followeduserid")]
+        public int FollowedId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User Follow { get; set; }
+
+        [ForeignKey("FollowedId")]
+        public User Followed { get; set; }
+    }
+}
