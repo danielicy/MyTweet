5d08ec5f05f1111d839a19f3f525e5c849b7b815
Danieli <danielic@razorgrip.com>
2019-01-20 18:03:09 +0200

    UserDto

--- server/DataModels/Dtos/UserDto.cs
+++ server/DataModels/Dtos/UserDto.cs
@@ -11,6 +11,6 @@ namespace DataModels.Dtos
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public FollowerDto[] Followed { get; set; }
+        public IList<FollowerDto> Contacts { get; set; }
     }
 }
