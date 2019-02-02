e6bf1c223e8b9c5763f70ddaa0be0aec8f6cb6a4
danieli <danielicy@hotmail.com>
2019-02-02 22:04:37 +0200

    home and misc

--- tweet-front/src/app/home/home.component.ts
+++ tweet-front/src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { User } from '../usermanagment/models/user';
+import { UsersService, AuthenticationService } from '../usermanagment/services';
+import { forEach } from '@angular/router/src/utils/collection';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  currentUser: User;
+  currentUserSubscription: Subscription;
+  users: User[] = [];
 
   constructor() { }
 
