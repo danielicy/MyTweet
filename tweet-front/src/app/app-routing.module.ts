03ce559b309efa67a08c5f858470c11bfe6f780a
Danieli <danielic@razorgrip.com>
2019-02-12 02:30:46 +0200

    rout to login

--- tweet-front/src/app/app-routing.module.ts
+++ tweet-front/src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent }];
+import { LoginComponent, RegisterComponent } from '../app/usermanagment/components';
+
+import { AuthGuard } from '../app/usermanagment/guards/auth.guard';
+
+ 
+ const routes: Routes = [
+   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+   { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tweet-front/src/app/usermanagment/services/users.service.ts
+++ tweet-front/src/app/usermanagment/services/users.service.ts
@@ -10,28 +10,26 @@ import { User } from '../models/user';
 export class UsersService {
 
   constructor(private http: HttpClient) { }
-  
+  appUrl = environment.appUrl;
 
   getAll() {
 
-    return this.http.get<User[]>(environment.appUrl + `/users`);
+    return this.http.get<User[]>(this.appUrl + `/users`);
   }
 
   getById(id: number) {
-    return this.http.get(environment.appUrl + `/users/${id}`);
+    return this.http.get(this.appUrl + `/users/${id}`);
   }
 
   register(user: User) {
-    return this.http.post(environment.appUrl + `/users/register`, user);
+    return this.http.post(this.appUrl + `/users/register`, user);
   }
 
   update(user: User) {
-    return this.http.put(environment.appUrl + `/users/${user.id}`, user);
+    return this.http.put(this.appUrl + `/users/${user.id}`, user);
   }
 
   delete(id: number) {
-    return this.http.delete(environment.appUrl + `
-
-/users/${id}`);
+    return this.http.delete(this.appUrl + `/users/${id}`);
   }
 }
